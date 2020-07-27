5b11c1e552baa8610f1f3fc637292cf3e1988571
Guillermo Lara <grlara9@gmail.com>
2020-07-26 22:34:05 -0700

    added dependencies and creates server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+var PORT = process.env.PORT || 5000;
+ 
+
+app.listen(PORT, ()=>{
+    console.log("Listening on PORT: " + PORT);
+})
