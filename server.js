950da649024e1e14b02d9c183cd1dfa3835b41e8
Guillermo Lara <grlara9@gmail.com>
2020-07-27 21:09:39 -0700

    added dependencies

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config();
 
+app.use(cors())
 app.use(express.json())
 
 var PORT = process.env.PORT || 5000;
