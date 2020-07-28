567b9c807dcaff2d47d392a9af67406b04f8ee9a
Guillermo Lara <grlara9@gmail.com>
2020-07-27 21:54:21 -0700

    create controllers folder

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config();
 
+var PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-var PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+mongoose.connect( uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log("MongoDB database connection established successfully")
+})
+
  
 
 app.listen(PORT, ()=>{
