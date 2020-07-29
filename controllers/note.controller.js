08a8e6ca2b0ad887ae0483f6ae0c4628fafcdff4
Guillermo Lara <grlara9@gmail.com>
2020-07-28 19:11:31 -0700

    added two new routes to get note and add new notes

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -1,4 +1,23 @@
 var express = require('express')
 var router = express.Router()
+var Note= require("../models/notes")
+
+router.get("/", (req, res)=>{
+    Note.find()
+    .then(response => res.json(response))
+    .catch(err=>res.status(400).json("Error: " + err))
+})
+
+router.post("/addnote", (req, res)=>{
+    const {title, note} = req.body
+    const newNote = new Note({
+        title,
+        note
+    })
+    newNote.save()
+    .then(()=> res.json("Note Added"))
+    .catch(err=>res.status(400).json("Error: " + err))
+})
+
 
 module.exports = router
