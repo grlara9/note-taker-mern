28cedee1a3264590435c6c49cb7db64915a8d6db
Guillermo Lara <grlara9@gmail.com>
2020-07-31 23:09:00 -0700

    initialized all pages with class components

---
+++ client/src/pages/add-note.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddNote extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this is the add note page</h1>
+            </div>
+        )
+    }
+}
---
+++ client/src/pages/all-notes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AllNotes extends Component {
+    render() {
+        return (
+            <div>
+                <h1>All notes pages to be display</h1>
+            </div>
+        )
+    }
+}
