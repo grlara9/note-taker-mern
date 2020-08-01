015f169d74306a44a56c516ccd630ebd49792016
Guillermo Lara <grlara9@gmail.com>
2020-07-31 23:42:59 -0700

    added react-router-dom to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import AllNotes from './pages/all-notes';
+import AddNote from './pages/add-note';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <Router>
+      <div className="App">
+      <Switch>
+        <Route exact path="/" component={AllNotes} />
+        <Route path="/addnotes" component={AddNote} />
+      </Switch>
     </div>
+    </Router>
+     
   );
 }
 
