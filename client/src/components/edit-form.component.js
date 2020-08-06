cf603c9a972a00e5df0783c216fc118e7be70dce
Guillermo Lara <grlara9@gmail.com>
2020-08-05 22:07:03 -0700

    create edit form

---
+++ client/src/components/edit-form.component.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+const EditForm = props => {
+    return (
+        <div className="form-container">
+        <form>
+           <div className="form-group">
+                <input 
+                    className="form-control"
+                    value={props.title}
+                    type="text"
+                    name="addnote"
+                    placeholder="Title"
+                    onChange={props.handleInputChange}
+                />
+                </div>
+
+                <div className="form-group">
+               
+                <textarea class="form-control col-sm-12" 
+                placeholder="Note...."
+                name="note" onChange={props.handleInputChange}
+                rows="3"></textarea>
+                </div>
+          
+          
+            <button type="submit" className="submitBtn btn btn-primary" onClick={props.handleFormSubmit}>
+                Submit
+            </button>
+        </form>
+        </div>
+    )
+}
+export default EditForm
