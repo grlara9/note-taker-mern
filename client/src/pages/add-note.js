f50db3ac780efd79e9e0f7a00e7ba00f36f16e57
Guillermo Lara <grlara9@gmail.com>
2020-08-08 21:23:42 -0700

    made an axios call to submit to mongodb

--- client/src/pages/add-note.js
+++ client/src/pages/add-note.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import FormAdd from '../components/new-note.component'
-
+import axios from 'axios';
 export default class AddNote extends Component {
     state={
         title:'',
@@ -21,11 +21,23 @@ export default class AddNote extends Component {
             note:value
         })
     }
+    handleFormSubmit = (e) =>{
+        e.preventDefault();
+        const values = {
+            title: this.state.title,
+            note:this.state.note
+        }
+        axios.post('http://localhost:5000/api/addnote', values)
+    }
 
     render() {
         return (
             <div>
-                <FormAdd onChangeTitle={this.onChangeTitle} onChangeNote={this.onChangeNote}/>
+                <FormAdd 
+                onChangeTitle={this.onChangeTitle} 
+                onChangeNote={this.onChangeNote}
+                handleFormSubmit={this.handleFormSubmit}
+                />
             </div>
         )
     } 
