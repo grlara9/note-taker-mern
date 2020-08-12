438c76b60fd7c629611d7f9558f5508f421e7487
Guillermo Lara <grlara9@gmail.com>
2020-08-11 20:34:19 -0700

    edit form style

--- client/src/components/new-note.component.js
+++ client/src/components/new-note.component.js
@@ -6,28 +6,27 @@ const FormAdd = props => {
         <div className="form-container">
                     <h1>{props.msg}</h1>
             <form onSubmit={(e) => {props.handleFormSubmit(e)}}>
-                <ul className="flex-outer">
-                    <li>
+                <div className="form-input">
                         <input 
-                            value={props.title}
                             type="text"
                             name="addnote"
                             placeholder="Title" 
+                            value={props.title}
                             onChange={(e) => {props.onChangeTitle(e.target.value)}}
                             />
-                    </li>
-                    <li>
+                   </div>
+                   <div className="form-input">
                         <textarea
                         value={props.note}
                             placeholder="Note...."
-                            name="note" onChange={(e) => {props.onChangeNote(e.target.value)}}
-                            rows="6"></textarea>
-                    </li>
-          
-                     <li>
+                            name="note" 
+                            onChange={(e) => {props.onChangeNote(e.target.value)}}
+                            rows="6"
+                            cols="30s"
+                            ></textarea>
+                  </div>
                         <button type="submit" >Submit</button>
-                    </li>
-                </ul>
+                   
             </form>
         </div>
     )
