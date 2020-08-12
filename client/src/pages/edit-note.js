76239de2e4babac522805958e5dbc4f2bcb637ce
Guillermo Lara <grlara9@gmail.com>
2020-08-11 20:47:44 -0700

    finish edit form

--- client/src/pages/edit-note.js
+++ client/src/pages/edit-note.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
-
+import EditForm from '../components/edit-form.component'
 import axios from 'axios';
 export default class EditNote extends Component {
 
     render() {
         return (
             <div>
-                <h1>edit</h1>
-                </div>
+                 <EditForm 
+                onChangeTitle={this.onChangeTitle} 
+                onChangeNote={this.onChangeNote}
+                handleFormSubmit={this.handleFormSubmit}
+                msg={this.state.msg}
+                />    
+            </div>
         )
     }
-}
+} 
