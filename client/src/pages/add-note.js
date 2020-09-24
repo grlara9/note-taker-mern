e8f8d0bffd06789aab3a8a34041e4ef5cc958f6e
Guillermo Lara <grlara9@gmail.com>
2020-09-23 23:09:24 -0700

    added new funtion to validate form

--- client/src/pages/add-note.js
+++ client/src/pages/add-note.js
@@ -7,10 +7,15 @@ export default class AddNote extends Component {
         this.state={
             title:'',
             note:'',
-            msg:''
+            msg:'',
+            fields: {},
+            errors: {}
         }
 
     }
+    handleValidation=()=>{
+        
+    };
    
     onChangeTitle= (value)=>{
         this.setState({
@@ -59,4 +64,49 @@ export default class AddNote extends Component {
             </div>
         )
     } 
+}
+this.state = {
+    fields: {},
+    errors: {}
+  }
+}
+
+handleValidation(){
+  let fields = this.state.fields;
+  let errors = {};
+  let formIsValid = true;
+
+  //Name
+  if(!fields["name"]){
+    formIsValid = false;
+    errors["name"] = "Cannot be empty";
+  }
+
+  if(typeof fields["name"] !== "undefined"){
+    if(!fields["name"].match(/^[a-zA-Z]+$/)){
+      formIsValid = false;
+      errors["name"] = "Only letters";
+    }      	
+  }
+
+  //Email
+  if(!fields["email"]){
+    formIsValid = false;
+    errors["email"] = "Cannot be empty";
+  }
+
+  if(typeof fields["email"] !== "undefined"){
+    let lastAtPos = fields["email"].lastIndexOf('@');
+    let lastDotPos = fields["email"].lastIndexOf('.');
+
+    if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+      formIsValid = false;
+      errors["email"] = "Email is not valid";
+    }
+  }
+
+
+
+  this.setState({errors: errors});
+  return formIsValid;
 }
