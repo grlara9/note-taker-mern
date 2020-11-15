7db82cf6aa40cc88f7fbbc228b9db058b3702daf
Guillermo Lara <grlara9@gmail.com>
2020-11-15 12:18:56 -0800

    finish formik form

--- client/src/pages/AddNotes.js
+++ client/src/pages/AddNotes.js
@@ -3,7 +3,7 @@ import { Formik } from "formik";
 
 const AddNote =()=>{
     return(
-        <Formik>
+        <Formik
              initialValues={{
           title: "",
           note: ""
@@ -16,8 +16,60 @@ const AddNote =()=>{
           }else if(values.name.length < 5){
               errors.title="Title should be more than 5 Characters"
           }
-          if()
+          if (values.note.trim().length < 1) {
+            errors.note = "Required";
+          }
+          else if(values.note.length < 15){
+            errors.note = "Note should be 15 characters long or more";
+          }
         }}
+        >
+            {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          setFieldValue,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              error={errors.title && touched.title}
+              label="Title "
+              variant="outlined"
+              type="title"
+              name="title"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.title}
+              fullWidth
+              className={classes.textField}
+            />
+            <TextField
+              variant="outlined"
+              label="Your phone number"
+              error={errors.phone && touched.phone}
+              type="phone"
+              name="phone"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.phone}
+              fullWidth
+              className={classes.textField}
+            />
+            <Button
+              type="submit"
+              disabled={!errors.name && !errors.phone}
+              variant="contained"
+              className={classes.textField}
+            >
+              Where's the Pizzaaaaaahhhh!!!!
+            </Button>
+            <div>{JSON.stringify({ ...values, ...errors })}</div>
+          </form>
+        )}
         </Formik>
     )
 }
+export default AddNote
