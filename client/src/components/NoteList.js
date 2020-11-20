2b1775af8fd7c86124e7a76885139c0b8b839771
Guillermo Lara <grlara9@gmail.com>
2020-11-19 20:03:22 -0800

    finish editing edit and delete button

--- client/src/components/NoteList.js
+++ client/src/components/NoteList.js
@@ -16,7 +16,7 @@ const NoteList  = props => {
             <div className="row">
                 {props.notes.map(note => { 
                     return (
-                        <div className="card col-md-3" key={note._id} >
+                        <div className="card col-md-4" key={note._id} >
                            
                             <div className="card-header">
                                 <h5>{note.title}</h5>
@@ -24,14 +24,12 @@ const NoteList  = props => {
                             <div className="card-body " >
                                 
                                 <p className="card-text">{note.note}</p>
-                                
+                            
                                 <Link to={"/edit/"+note._id} ><button className="button button-edit">Edit</button></Link>
                                 
                                 <a href="/"><button className="button button-delete" onClick={() => { props.delete(note._id) }}>Delete</button></a>
-                                
+                                </div>
                             </div>
-                        
-                        </div>
                     );                
                 })} 
             </div>  
